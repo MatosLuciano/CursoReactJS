e9437bb7e416f00aafee322e12fb82ad2293febc
Luciano Matos <luciano7matos@gmail.com>
2021-05-30 22:20:22 -0300

    Add files via upload

---
+++ src/componentes/Resultado.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Resultado(props) {
+   return(
+    <>
+        <div>
+            <p>Soma das notas: {props.somaNotas}</p>
+            <p>{props.somaNotas >= 60 ? "Aprovado" : "Reprovado"}</p>
+            <p></p>
+            <p></p>
+        </div>  
+    </>
+  )
+}
